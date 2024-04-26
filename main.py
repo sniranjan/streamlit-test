1761484d5c26778781d1e81691c2b8aa888ae09d
sniranjan <niranjansonachalam@gmail.com>
2024-04-26 10:05:06 -0500

    initial commit

---
+++ main.py
@@ -0,0 +1,7 @@
+## Simple Streamlit app that displays a 2 column layout with a title and a text
+
+import streamlit as st
+
+st.title('My first Streamlit app')
+st.write('Here is a simple example of a Streamlit app with a title and some text.')
+
